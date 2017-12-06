2be0adb6d7a6ae81e2931db1364836b4f3dace41
webnux <kiruban.premkumar@gmail.com>
2017-12-06 22:59:52 +0100

    Update cron.rb

--- cron.rb
+++ cron.rb
@@ -31,17 +31,40 @@ last = client.public.ticker('XXBTZEUR')['XXBTZEUR']['c'][0].to_f.floor2(2)
 high = client.public.ticker('XXBTZEUR')['XXBTZEUR']['h'][1].to_f.floor2(2)
 avg = client.public.ticker('XXBTZEUR')['XXBTZEUR']['p'][1].to_f.floor2(2)
 
-moving_pv = (high-low)/high*100
-buy_pv = moving_pv/3
-sell_pv = moving_pv/2
-security_pv = 1
 
-if client.private.open_orders["open"].empty? && wallet_euro > 100
-  if last < avg
-    pv = (avg-last)/last*100
-    if pv > security_pv && pv > buy_pv
-      volume = wallet_euro/last
-      volume = volume.floor2(4)
+total_cash = wallet_euro + wallet_xbt*last
+position_rate_euro = total_cash / 10
+position_rate_xbt = (total_cash/last) / 10
+
+risk = 2.0 # <=> 2%
+
+# update bid price
+
+if wallet_euro < position_rate_euro
+  last_order = File.open("bidprice.txt", "r")
+  last_bidprice = last_order.read.to_f
+  pv = (last-last_bidprice)/last_bidprice*100
+  if pv > risk
+    file = File.open("bidprice.txt","w")
+    file.puts last
+    file.close
+  end
+end
+
+# Buy per step
+if client.private.open_orders["open"].empty? && wallet_euro > 0
+  if last > avg
+    last_order = File.open("bidprice.txt", "r")
+    last_bidprice = last_order.read.to_f
+    pv = (last-last_bidprice)/last_bidprice*100
+    if pv > risk
+      if position_rate_euro <= wallet_euro
+        volume = position_rate_euro/last
+        volume = volume.floor2(4)
+      else
+        volume = wallet_euro/last
+        volume = volume.floor2(4)
+      end
       opts = {pair: 'XBTEUR',type: 'buy',ordertype: 'market', volume: volume}
       client.private.add_order(opts)
       file = File.open("bidprice.txt","w")
@@ -55,21 +78,28 @@ if client.private.open_orders["open"].empty? && wallet_euro > 100
   end
 end
 
-if client.private.open_orders["open"].empty? && wallet_euro < 100
-  file = File.open("bidprice.txt", "r")
-  bidprice = file.read
-  file.close
-  bidprice = bidprice.to_f
-  if last > bidprice
-    pv = (last-bidprice)/bidprice*100
-    if pv > security_pv && pv > sell_pv
-      opts = {pair: 'XBTEUR',type: 'sell',ordertype: 'market', volume: wallet_xbt}
-      client.private.add_order(opts)
-      puts "Vente"
-      puts last
-      puts avg
-      puts opts
+# Sell by step
+if client.private.open_orders["open"].empty? && wallet_xbt > 0
+  last_order = File.open("bidprice.txt", "r")
+  last_bidprice = last_order.read.to_f
+  pv = (last-last_bidprice)/last_bidprice*100
+  if pv < risk
+    if position_rate_xbt <= wallet_xbt
+      volume = position_rate_xbt
+      volume = volume.floor2(4)
+    else
+      volume = wallet_xbt
+      volume = volume.floor2(4)
     end
+    opts = {pair: 'XBTEUR',type: 'sell',ordertype: 'market', volume: volume}
+    client.private.add_order(opts)
+    file = File.open("bidprice.txt","w")
+    file.puts last
+    file.close
+    puts "Vente"
+    puts last
+    puts avg
+    puts opts
   end
 end
 
